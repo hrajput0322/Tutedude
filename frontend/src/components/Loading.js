f81d193b586235a2a8d297812e57c046d52100b9
hrajput0322 <hrajput0322@gmail.com>
2024-09-16 19:32:03 +0530

    loader styled

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Loading.css';
 
 const Loading = () => {
     return (
-        <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+        <div className="d-flex justify-content-center align-items-center main-container" style={{ height: "100vh" }}>
             <div className="spinner-border text-primary" role="status">
-                <span className="sr-only">Loading...</span>
+                <span className="sr-only"></span>
             </div>
         </div>
     );
